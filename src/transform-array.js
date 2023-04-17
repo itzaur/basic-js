c245913ff7b2eee9fca7049f94451744a44bf8d4
itzaur <preneur85@gmail.com>
2023-04-18 01:44:40 +0300

    feat: add transform-arrays some tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require("../extensions/index.js");
+// const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,27 +13,32 @@ const { NotImplementedError } = require("../extensions/index.js");
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/*arr*/) {
-  throw new NotImplementedError("Not implemented");
-  // let cloneArray = structuredClone(arr)
-  // const discardNextIndex = arr.indexOf("--discard-next");
-  // const discardPrevIndex = arr.indexOf("--discard-prev");
-  // const doubleNextIndex = arr.indexOf("--double-next");
-  // const doublePrevIndex = arr.indexOf("--double-prev");
-  // console.log(discardNextIndex);
-  // let result = []
-  // // for(let i = 0; i < cloneArray.length; i++) {
-  //   arr.splice(discardNextIndex + 1, 1);
-  //   arr.splice(doublePrevIndex - 1, 0, arr[doublePrevIndex - 1]);
-  //   // arr.splice(doubleNextIndex + 1, 0, arr[doubleNextIndex])
-  // // }
-  // return arr
-}
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error(`'arr' parameter must be an instance of the Array!`);
+  }
 
-// console.log((transform([1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5]))); //[1, 2, 3, 4, 5]
-// console.log(transform([1, 2, 3, "--double-next", 1337, "--double-prev", 4, 5])); //[1, 2, 3, 1337, 1337, 1337, 4, 5]
-// console.log(transform([1, 2, 3, "--discard-next", 1337, "--discard-prev", 4, 5])); //[1, 2, 3, 4, 5]
-// console.log(transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5])); //[1, 2, 3, 1337, 4, 5]
+  return arr.flatMap((item, index, arr) => {
+    console.log(item);
+    if (arr.length === 0) return [];
+
+    if (
+      arr[index - 1] === "--discard-next" ||
+      arr[index + 1] === "--discard-prev"
+    )
+      return [];
+
+    if (
+      arr[index - 1] === "--double-next" ||
+      arr[index + 1] === "--double-prev"
+    )
+      return [item, item];
+
+    if ((item + "").includes("--")) return [];
+
+    return item;
+  });
+}
 
 module.exports = {
   transform,
